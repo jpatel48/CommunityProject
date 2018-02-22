78447a9879d7d7e86bd276f9d9cc8c9ef378dfdd
pbassan1 <36545634+pbassan1@users.noreply.github.com>
2018-02-21 23:46:49 -0500

    migration updateDatabase
    i did migarations and update database. and working on position.

--- SampleProjectBootStrap-enhanced/SampleProjectBootStrap/DAL/JobEntities.cs
+++ SampleProjectBootStrap-enhanced/SampleProjectBootStrap/DAL/JobEntities.cs
@@ -14,7 +14,7 @@ namespace SampleProjectBootStrap.DAL
         public DbSet<Application> Applications { get; set; }
         public DbSet<Position> Positions { get; set; }
         public DbSet<Posting> Postings { get; set; }
-        public DbSet<Occupation> Occupations { get; set; }
+       // public DbSet<Occupation> Occupations { get; set; }
         public DbSet<Location> Locations { get; set; }
         public DbSet<Qualification> Qualifications { get; set; }
         
@@ -29,11 +29,12 @@ namespace SampleProjectBootStrap.DAL
 
         public System.Data.Entity.DbSet<SampleProjectBootStrap.Models.Department> Departments { get; set; }
 
-        public System.Data.Entity.DbSet<SampleProjectBootStrap.Models.City> Cities { get; set; }
+       // public System.Data.Entity.DbSet<SampleProjectBootStrap.Models.City> Cities { get; set; }
 
-        public System.Data.Entity.DbSet<SampleProjectBootStrap.Models.Province> Provinces { get; set; }
-        public System.Data.Entity.DbSet<SampleProjectBootStrap.Models.FTE> FTEs { get; set; }
+        //public System.Data.Entity.DbSet<SampleProjectBootStrap.Models.Province> Provinces { get; set; }
+       // public System.Data.Entity.DbSet<SampleProjectBootStrap.Models.FTE> FTEs { get; set; }
         public System.Data.Entity.DbSet<SampleProjectBootStrap.Models.JobCode> JobCodes { get; set; }
 
+        public System.Data.Entity.DbSet<SampleProjectBootStrap.Models.Compensation> Compensations { get; set; }
     }
 }
